ab6b8f42d123d587ce95acffa3fb33783ec17600
Malyssa Winters <malyssawinters5@gmail.com>
2016-07-21 10:11:26 -0400

    initial commit

---
+++ DressItDB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DressItDB.Startup))]
+namespace DressItDB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
